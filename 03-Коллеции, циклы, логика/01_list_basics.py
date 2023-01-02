5da88a7a8b5612657b8d8f28d08b3fb09cc18959
web-webalexx <web-webalexx@yandex.ru>
2023-01-02 12:01:32 +0100

    updated

--- 03-Коллеции, циклы, логика/01_list_basics.py
+++ 03-Коллеции, циклы, логика/01_list_basics.py
@@ -4,20 +4,17 @@
 # In[1]:
 
 
-int_list = [1,2,3]
-
+int_list = [10, 1, 2, 3]
 
 # In[2]:
 
 
 mixed_list = [1, 2.0, 'string']
 
-
 # In[3]:
 
 
 len(int_list)
-
 
 # In[4]:
 
@@ -25,12 +22,11 @@ len(int_list)
 print(int_list[0])
 print(int_list[-1])
 
-
 # In[5]:
 
 
 int_list[1:]
-
+# int_list[:1]
 
 # In[6]:
 
@@ -40,13 +36,11 @@ names2 = ['Tracy', 'Elijah', 'Mason']
 
 names1 + names2
 
-
 # In[7]:
 
 
 names1[0] = 'Liam'
 names1
-
 
 # In[8]:
 
@@ -55,14 +49,12 @@ names1.append('William')
 names1.append('James')
 names1
 
-
 # In[9]:
 
 
-popped = names1.pop() #same as names1.pop(-1)
+popped = names1.pop()  # same as names1.pop(-1)
 print(popped)
 print(names1)
-
 
 # In[10]:
 
@@ -70,14 +62,12 @@ print(names1)
 names1.pop(0)
 names1
 
-
 # In[11]:
 
 
 names1.append('James')
 names1.sort()
 names1
-
 
 # In[21]:
 
@@ -90,7 +80,6 @@ letters = ['abc', 'a', 'ab']
 letters.sort(key=len)
 print(letters)
 
-
 # In[23]:
 
 
@@ -99,23 +88,20 @@ result = sorted(letters)
 print(type(result))
 print(result)
 
-
 # In[1]:
 
 
-numbers = [3,2,8,5,0,3,4,1]
+numbers = [3, 2, 8, 5, 0, 3, 4, 1]
 print(numbers)
-numbers.sort() #actually it is Timsort
+numbers.sort()  # actually it is Timsort
 print(numbers)
-
 
 # In[14]:
 
 
-numbers = [3,2,8,5,0,3,4,1]
+numbers = [3, 2, 8, 5, 0, 3, 4, 1]
 numbers.reverse()
 numbers
-
 
 # In[15]:
 
@@ -123,25 +109,21 @@ numbers
 numbers.sort(reverse=True)
 numbers
 
-
 # In[16]:
 
 
 numbers.insert(0, 22)
 numbers
 
-
 # In[2]:
 
 
 print(numbers.index(42))
 
-
 # In[ ]:
 
 
 print(numbers.count(3))
-
 
 # In[ ]:
 
@@ -149,10 +131,8 @@ print(numbers.count(3))
 copy = numbers.copy()
 print(copy)
 
-
 # In[ ]:
 
 
 numbers.clear()
 numbers
-
